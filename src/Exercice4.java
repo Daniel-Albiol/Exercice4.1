8c10a34924457170b2057db4360ff39dde76887f
datst <alualb932@ieselcaminas.org>
2021-10-23 12:58:36 +0200

    Initial commit

---
+++ src/Exercice4.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class Exercice4 {
+    public static void main(String[] argv) {
+        float x;
+        float y;
+        float addition;
+        float subtraction;
+        float multiplication;
+        float division;
+        int operation;
+        System.out.println("Enter the num1:");
+//Reading the value
+        Scanner inputValue;
+        inputValue=new Scanner(System.in);
+        x = inputValue.nextFloat();
+        /////////////
+        System.out.println("Enter the num552:");
+//Reading the value2
+        Scanner num;
+        num=new Scanner(System.in);
+        y = num.nextFloat();
+        addition = x + y;
+        subtraction = x-y;
+        multiplication = x*y;
+        division= x/y;
+        System.out.println("La suma es: " +addition);
+        System.out.println("La suma es: " +subtraction);
+        System.out.println("La suma es: " +multiplication);
+        System.out.println("La suma es: " +division);
+
+
+    }
+}
